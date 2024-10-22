4c722e52569dd853592aa3bb5ad225a1fc597321
FatmaMehreen <fatmamehreen2005@gmail.com>
2024-10-22 10:09:17 +0400

    Create week2.c

---
+++ week2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+ 
+#define coffeebeans 8
+#define water 30
+#define milkforespresso 0
+#define milkforcappucino 70
+#define milkformocha 160
+#define chocformocha 30
+#define waterformocha 39
+#define adminpassword 00000
+#define coffeethreshold 8
+#define waterthreshold 30
+#define chocthreshold 
+#define milkthreshold
+
+int askordertype();
+int  exitapplication();
+int adminoperation();
+
+
+int main(){
+
+return 0;
+
+
+}
+
+
+int askordertype() {
+}
+
+
+int exitapplication(){
+}
+
+int adminoperation(){
+}
