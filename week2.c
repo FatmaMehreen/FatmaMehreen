00f7e6e44af6cea6c62c2891f740681f96418f18
FatmaMehreen <fatmamehreen2005@gmail.com>
2024-10-22 09:44:20 +0400

    Update week2.c

--- week2.c
+++ week2.c
@@ -24,74 +24,17 @@ int pricelist();
 
 int main(){
 
-int askuserchoice;
-
-while(1){
-  printf("Hello! I'm Lily your coffee server! \n");
-  printf("Please select one: \n1. Admin Mode \n2. Order coffee customer \n3. exit \n");
-  scanf("%d", &askuserchoice);
-
-
-  if (askuserchoice == 1){
-    adminoperation();
-  } else if (askuserchoice == 2){
-    menu();
-    askordertype();
-  } else if (askuserchoice == 3){
-     exitapplication();
-   break;
-   }
-    else {
-    printf("incorrect input. Please try again");
-  }
-
 return 0;
 
+
 }
-}
 
-
-/*int askordertype(){
-  int order;
-  printf("Please select your order: 1. espresso 2. cappucino 3. mocha");
-  scanf("%d", &order);
-  if (order== 1){
-    printf("You have ordered espresso. confirm?(1.yes 2. no)");}
-    else if (order == 2){
-      printf("You have ordered cappucino. confirm?(1. yes 2. no)");
-    } else if (order == 3){
-      printf("you have ordered mocha. confirm?1. yes 2 no");
-
-    }
-    }*/
 
 int askordertype() {
     int order;
     int confirm;
-
-    printf("Please select your order: 1. espresso 2. cappuccino 3. mocha\n");
+ printf("Please select your order: 1. espresso 2. cappuccino 3. mocha\n");
     scanf("%d", &order);
-
-    if (order == 1) {
-        printf("You have ordered espresso. Confirm? (1. yes 2. no)\n");
-    } else if (order == 2) {
-        printf("You have ordered cappuccino. Confirm? (1. yes 2. no)\n");
-    } else if (order == 3) {
-        printf("You have ordered mocha. Confirm? (1. yes 2. no)\n");
-    } else {
-        printf("Invalid order. Please try again.\n");
-        return 0;  // Return 0 for invalid input
-    }
-
-    scanf("%d", &confirm);
-    if (confirm == 1) {
-        printf("Order confirmed!\n");
-        printf("Please proceed to pay");
-        paymentmethod();
-    } else {
-        printf("Order canceled.\n");
-        return 0;  // Return 0 for canceled order
-    }
 }
 
 
@@ -101,17 +44,6 @@ printf("the items are espresso, cappucino, mocha\n");
 printf("the price for espresso is 3.5\n");
 printf("the price for cappucino is 4.5\n");
 printf("the price for mocha is 5.5\n");
-/*printf("Do you wish to see the prices for the items?(1.Yes 2. No)");
-scanf("%d", &priceview);
-if (priceview == 1){
-  pricelist();
-} else if (priceview == 2){
-  printf("Please proceed to pay for the items");
-  paymentmethod();
-} else {
-  printf("Please try again");
-}*/
-askordertype();
 }
 
 
