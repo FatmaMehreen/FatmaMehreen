5d028f4d3c75b20718024b89f1fea7cf8fabb707
FatmaMehreen <fatmamehreen2005@gmail.com>
2024-10-22 09:54:18 +0400

    Update week3.c

--- week3.c
+++ week3.c
@@ -14,40 +14,19 @@
 #define chocthreshold 
 #define milkthreshold
 
-int menu();
 int askordertype();
 int  exitapplication();
-int paymentmethod();
 int adminoperation();
 int pricelist();
 
 
 int main(){
-
-int askuserchoice;
-
-while(1){
+ 
   printf("Hello! I'm Lily your coffee server! \n");
   printf("Please select one: \n1. Admin Mode \n2. Order coffee customer \n3. exit \n");
   scanf("%d", &askuserchoice);
-
-
-  if (askuserchoice == 1){
-    adminoperation();
-  } else if (askuserchoice == 2){
-    menu();
-    askordertype();
-  } else if (askuserchoice == 3){
-     exitapplication();
-   break;
-   }
-    else {
-    printf("incorrect input. Please try again");
-  }
-
+ 
 return 0;
-
-}
 }
 
 
@@ -95,25 +74,6 @@ int askordertype() {
 }
 
 
-int menu(){
-//int priceview;
-printf("the items are espresso, cappucino, mocha\n");
-printf("the price for espresso is 3.5\n");
-printf("the price for cappucino is 4.5\n");
-printf("the price for mocha is 5.5\n");
-/*printf("Do you wish to see the prices for the items?(1.Yes 2. No)");
-scanf("%d", &priceview);
-if (priceview == 1){
-  pricelist();
-} else if (priceview == 2){
-  printf("Please proceed to pay for the items");
-  paymentmethod();
-} else {
-  printf("Please try again");
-}*/
-askordertype();
-}
-
 
 /*int pricelist(){
 int priceespresso = 3.5;
@@ -127,30 +87,6 @@ paymentmethod();
 }*/
 
 
-int paymentmethod(){
-  int payment;
-  int coins;
-  printf("please select your payment method:\n 1. Debit/Credit Card\n 2. Coins\n ");
-  scanf("%d", &payment);
-  if ( payment == 1){
-    printf("pay by card");
-    printf("Thank you!Have a good day!");
-    } else if (payment == 2 ){
-      while(1){
-        printf("pay by coins (1. 1 dhs 2. 0.5 dhs)");
-        if (coins == 1){
-          printf("Thank you! Have a good day!");
-        } else if (coins == 1){
-          printf("Thank you! havea good day!");
-          break;
-        } else {
-          printf("Invalid. try using only 1 or 0.5 dhs coins only");
-        }
-        }
-;
-    }
-    
-  }
 
 int adminoperation(){
 int password;
